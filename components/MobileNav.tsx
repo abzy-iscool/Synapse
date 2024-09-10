d5eaaa62d819291fd534dfa4bf61a35ac8a322ed
Abhishek J <abhishekjani960@gmail.com>
2024-09-10 14:14:03 +0300

    Update MobileNav.tsx

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { SynapseLogo } from '../public/logos/SynapseLogo'
+import { SynapseLogo } from '../public/logos/SynapseLogo.jsx'
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
